6eb21d05db2bb42c0b3482dbc1f28a3399d4c996
raphael.schott <raphael07.schott@gmail.com>
2024-11-17 13:16:26 -0500

    gestion any build issue

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ interface DataItem {
   saison: string;
   prix: number;
   niveau: string;
-  [key: string]: any; 
+  [key: string]: string | number | boolean; 
 }
 
 export default function Home() {
